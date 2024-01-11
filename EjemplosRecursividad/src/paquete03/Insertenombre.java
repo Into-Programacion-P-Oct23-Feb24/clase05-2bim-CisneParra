e7c738992bf0693b08ad7c1af1fc5c8bb44387d8
Cisne Parra <adparra2@utpl.edu.ec>
2024-01-11 15:51:38 -0500

    cambios de la clase

---
+++ EjemplosRecursividad/src/paquete03/Insertenombre.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+/**
+ *
+ * @author natsu
+ */
+public class Insertenombre {
+      public static void main(String[] args) {
+          
+     }
+      public static String [][] obtenerArreglo (){
+          String[][] arreglo = {{"LOJA","CUENCA","QUITO"},{"GUAYAQUIL","ZAMORA",
+              "MACHALA"}};
+          return arreglo;
+      }
+}
